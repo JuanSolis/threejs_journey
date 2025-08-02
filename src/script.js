28b5e469eab3231a72f27ffaf4cdbd34617aaeb9
JuanSolis <juansolisgtsk@hotmail.com>
2025-08-02 17:54:30 -0600

    Se Agregaron modelos a la escena

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { bool } from "three/src/nodes/TSL.js";
 
 /**
  * Base
@@ -17,15 +19,16 @@ const scene = new THREE.Scene();
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
 
 gui.add(ambientLight, "intensity").min(0).max(3).step(0.001);
 
 const pointLight = new THREE.PointLight(0xffffff, 50);
-pointLight.position.x = 1;
+pointLight.position.x = -1;
 pointLight.position.y = 3;
-pointLight.position.z = 4;
+pointLight.position.z = -4;
+pointLight.visible = true;
 scene.add(pointLight);
 
 const pointLightHelper = new THREE.PointLightHelper(pointLight);
@@ -33,31 +36,69 @@ scene.add(pointLightHelper);
 
 const guiPointLightFolder = gui.addFolder("PointLight");
 guiPointLightFolder.add(pointLight.position, "x").min(-5).max(5).step(0.5);
+guiPointLightFolder.add(pointLight.position, "y").min(-5).max(5).step(0.5);
+guiPointLightFolder.add(pointLight.position, "z").min(-5).max(5).step(0.5);
+
+const directionalLight = new THREE.DirectionalLight(0xff91c499, 2.5);
+directionalLight.position.set(2, 1, 2);
+
+scene.add(directionalLight);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight
+);
+scene.add(directionalLightHelper);
+
+const guiDirectionalLigthFolder = gui.addFolder("Directional Light");
+
+guiDirectionalLigthFolder
+  .add(directionalLight.position, "x")
+  .min(-5)
+  .max(5)
+  .step(0.5);
+
+guiDirectionalLigthFolder
+  .add(directionalLight.position, "y")
+  .min(-5)
+  .max(5)
+  .step(0.5);
+
+guiDirectionalLigthFolder
+  .add(directionalLight.position, "z")
+  .min(-5)
+  .max(5)
+  .step(0.5);
 
 /**
  * Objects
  */
 // Material
-const material = new THREE.MeshStandardMaterial();
-material.roughness = 0.4;
 
-// Objects
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
-sphere.position.x = -1.5;
+const loader = new GLTFLoader();
+loader.load(
+  "models/LeePerrySmith.glb", // or .glb
+  function (gltf) {
+    const model = gltf.scene;
+    scene.add(model);
 
-const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
-
-const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.3, 0.2, 32, 64),
-  material
+    model.scale.set(0.5, 0.5, 0.5);
+    model.traverse(function (object) {
+      if (object.isMesh) object.castShadow = true;
+    });
+    const skeleton = new THREE.SkeletonHelper(model);
+    skeleton.material.linewidth = 5;
+    skeleton.visible = true;
+    scene.add(skeleton);
+  },
+  function (xhr) {
+    // Optional progress callback: called while the model is loading
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  function (error) {
+    // Optional error callback: called if there's an error loading the model
+    console.error("An error occurred loading the model:", error);
+  }
 );
-torus.position.x = 1.5;
-
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
-plane.rotation.x = -Math.PI * 0.5;
-plane.position.y = -0.65;
-
-scene.add(sphere, cube, torus, plane);
 
 /**
  * Sizes
@@ -118,13 +159,6 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  sphere.rotation.y = 0.1 * elapsedTime;
-  cube.rotation.y = 0.1 * elapsedTime;
-  torus.rotation.y = 0.1 * elapsedTime;
-
-  sphere.rotation.x = 0.15 * elapsedTime;
-  cube.rotation.x = 0.15 * elapsedTime;
-  torus.rotation.x = 0.15 * elapsedTime;
 
   // Update controls
   controls.update();
